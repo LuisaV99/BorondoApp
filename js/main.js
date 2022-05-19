62b9c69e496b5ebfe2ca0329064b805361ba1e64
Luisa <60489324+LuisaV99@users.noreply.github.com>
2022-05-19 10:17:47 -0500

    Registro
    Cargue archivos Formulario Registro. Versión 14 mayo

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
 
-
-var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
-
-btnAbrirPopup.addEventListener('click', function(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
-});
-
-btnCerrarPopup.addEventListener('click', function(e){
-	e.preventDefault();
-	overlay.classList.remove('active');
-	popup.classList.remove('active');
-});
-
-
-
 (function ($) {
     "use strict";
 
@@ -73,11 +54,4 @@ btnCerrarPopup.addEventListener('click', function(e){
     
     
 
-    
-
-
-
-
-
 })(jQuery);
-
