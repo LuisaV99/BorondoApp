c4af4a2535015a91dcdc9bd38fa8b6548a55ca02
Luisa <60489324+LuisaV99@users.noreply.github.com>
2022-05-19 09:19:57 -0500

    Login
    Cargue archivos Login. Ultima versión

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 
+
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+
+
+
 (function ($) {
     "use strict";
 
@@ -54,4 +73,11 @@
     
     
 
+    
+
+
+
+
+
 })(jQuery);
+
